99600d442fd445f0ed1d251596809b71224fae6a
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2017-08-08 13:43:46 +0300

    The "from" declaration was removed from truffle.js Added an unit tests that check the Migration contract

---
+++ test/utils/Migrations.test.js
@@ -0,0 +1,33 @@
+const Migrations = global.artifacts.require('Migrations.sol');
+
+
+global.contract('Migrations', (accounts) => {
+  let migrationsContract;
+
+  const owner1 = accounts[0];
+  const owner2 = accounts[0];
+
+  global.beforeEach(async () => {
+    migrationsContract = await Migrations.new({ from: owner1 });
+  });
+
+  global.it('check Migrations contract', async () => {
+    global.console.log(`\tmigrationsContract: ${migrationsContract.address}`);
+    global.assert.equal(migrationsContract.address == 0x0, false);
+
+    let contractOwner = await migrationsContract.owner.call();
+    global.assert.equal(contractOwner, owner2);
+
+    let lastCompletedMigration = await migrationsContract.last_completed_migration.call();
+    global.assert.equal(lastCompletedMigration.toNumber(), 0);
+
+    await migrationsContract.setCompleted.sendTransaction(1, { from: owner1 });
+    lastCompletedMigration = await migrationsContract.last_completed_migration.call();
+    global.assert.equal(lastCompletedMigration.toNumber(), 1);
+
+    await migrationsContract.setCompleted.sendTransaction(2, { from: owner2 });
+    lastCompletedMigration = await migrationsContract.last_completed_migration.call();
+    global.assert.equal(lastCompletedMigration.toNumber(), 1,
+      'It should not be changed the last_completed_migration if setCompleted called by any account other than the owner');
+  });
+});
