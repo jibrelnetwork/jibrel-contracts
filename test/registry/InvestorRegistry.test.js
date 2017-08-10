93077e699dc566f70c5ee4ac0f9d40f4035b1fba
Victor Mezrin <victor@mezrin.com>
2017-08-10 12:28:19 +0000

    fix InvestorRegistry.test.js

--- test/registry/InvestorRegistry.test.js
+++ test/registry/InvestorRegistry.test.js
@@ -1,34 +1,35 @@
 /* eslint-disable no-multi-spaces */
-const InvestorRegistry      = global.artifacts.require('InvestorRegistry.sol');
+const InvestorRegistry = global.artifacts.require('InvestorRegistry.sol');
+
+const ManageableRoutines = require('../../routine/Manageable');
+
 
 global.contract('InvestorRegistry', (accounts) => {
-  const owner = accounts[0];
-  const manager = accounts[1];
+  const owner      = accounts[0];
+  const manager    = accounts[1];
   const investor01 = accounts[3];
 
   let InvestorRegistryContract;
 
   global.beforeEach(async () => {
     InvestorRegistryContract = await InvestorRegistry.new({ from: owner });
+    await ManageableRoutines.enableManager(InvestorRegistryContract.address, owner, manager);
+    await ManageableRoutines.grantManagerPermissions(InvestorRegistryContract.address,
+                                                     owner, manager, ['admit_investor', 'deny_investor']);
   });
 
   global.it('check InvestorRegistry contract', async () => {
-
     global.console.log(`\tInvestorRegistryContract: ${InvestorRegistryContract.address}`);
     global.assert.equal(InvestorRegistryContract.address === 0x0, false);
-
-    await InvestorRegistryContract.grantManagerPermission.sendTransaction(manager, 'admit_investor', { from: owner });
-    const isManagerAllowed = await InvestorRegistryContract.isManagerAllowed.call(manager, 'admit_investor');
-    global.assert.equal(isManagerAllowed, false);
 
     let isInvestorAdmitted = await InvestorRegistryContract.isInvestorAdmitted.call(investor01);
     global.assert.equal(isInvestorAdmitted, false);
 
-    await InvestorRegistryContract.admitInvestor.sendTransaction(investor01, { from: owner });
+    await InvestorRegistryContract.admitInvestor.sendTransaction(investor01, { from: manager });
     isInvestorAdmitted = await InvestorRegistryContract.isInvestorAdmitted.call(investor01);
     global.assert.equal(isInvestorAdmitted, true);
 
-    await InvestorRegistryContract.denyInvestor.sendTransaction(investor01, { from: owner });
+    await InvestorRegistryContract.denyInvestor.sendTransaction(investor01, { from: manager });
     isInvestorAdmitted = await InvestorRegistryContract.isInvestorAdmitted.call(investor01);
     global.assert.equal(isInvestorAdmitted, false);
   });
