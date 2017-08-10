6705ded3c8a1efba71aba4c1ef6b825093b0a70c
Victor Mezrin <victor@mezrin.com>
2017-08-10 12:03:20 +0000

    fix Migrations.test.js

--- test/utils/Migrations.test.js
+++ test/utils/Migrations.test.js
@@ -4,30 +4,29 @@ const Migrations = global.artifacts.require('Migrations.sol');
 global.contract('Migrations', (accounts) => {
   let migrationsContract;
 
-  const owner1 = accounts[0];
-  const owner2 = accounts[0];
+  const owner   = accounts[0];
+  const nonOwner = accounts[1];
 
   global.beforeEach(async () => {
-    migrationsContract = await Migrations.new({ from: owner1 });
+    migrationsContract = await Migrations.new({ from: owner });
   });
 
   global.it('check Migrations contract', async () => {
     global.console.log(`\tmigrationsContract: ${migrationsContract.address}`);
-    global.assert.equal(migrationsContract.address == 0x0, false);
+    global.assert.equal(migrationsContract.address === 0x0, false);
 
-    let contractOwner = await migrationsContract.getOwner.call();
-    global.assert.equal(contractOwner, owner2);
+    const contractOwner = await migrationsContract.getOwner.call();
+    global.assert.equal(contractOwner, owner);
 
     let lastCompletedMigration = await migrationsContract.getLastCompletedMigration.call();
     global.assert.equal(lastCompletedMigration.toNumber(), 0);
 
-    await migrationsContract.setCompleted.sendTransaction(1, { from: owner1 });
+    await migrationsContract.setCompleted.sendTransaction(1, { from: owner });
     lastCompletedMigration = await migrationsContract.getLastCompletedMigration.call();
     global.assert.equal(lastCompletedMigration.toNumber(), 1);
 
-    await migrationsContract.setCompleted.sendTransaction(2, { from: owner2 });
+    await migrationsContract.setCompleted.sendTransaction(2, { from: nonOwner });
     lastCompletedMigration = await migrationsContract.getLastCompletedMigration.call();
-    global.assert.equal(lastCompletedMigration.toNumber(), 1,
-      'It should not be changed the last_completed_migration if setCompleted called by any account other than the owner');
+    global.assert.equal(lastCompletedMigration.toNumber(), 1, 'Only owner should be able to change completed migration ');
   });
 });
