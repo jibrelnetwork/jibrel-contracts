4e31bef1d20f69a7f2d549fcae70840c12a7a1fc
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2017-08-31 23:14:32 +0300

    Convert functions to external JibrelAPI

--- contracts/api/JibrelAPI.sol
+++ contracts/api/JibrelAPI.sol
@@ -22,10 +22,10 @@ contract JibrelAPIInterface {
 
   /* Public functions */
 
-  function getBODC() constant returns (address);
-  function getJibrelDAO() constant returns (address);
-  function getInvestorRegistry() constant returns (address);
-  function getCryDRRegistry() constant returns (address);
+  function getBODC() external constant returns (address);
+  function getJibrelDAO() external constant returns (address);
+  function getInvestorRegistry() external constant returns (address);
+  function getCryDRRegistry() external constant returns (address);
 }
 
 
@@ -66,22 +66,22 @@ contract JibrelAPI is JibrelAPIInterface, Manageable {
 
   /* Setters */
 
-  function setBODC(address _newAddress) checkAddress(_newAddress) onlyAllowedManager('set_bodc') {
+  function setBODC(address _newAddress) checkAddress(_newAddress) external onlyAllowedManager('set_bodc') {
     BODC = _newAddress;
     BODCChangedEvent(_newAddress);
   }
 
-  function setJibrelDAO(address _newAddress) checkAddress(_newAddress) onlyAllowedManager('set_jibrel_dao') {
+  function setJibrelDAO(address _newAddress) checkAddress(_newAddress) external onlyAllowedManager('set_jibrel_dao') {
     JibrelDAO = _newAddress;
     JibrelDAOChangedEvent(_newAddress);
   }
 
-  function setInvestorRegistry(address _newAddress) checkAddress(_newAddress) onlyAllowedManager('set_investor_repo') {
+  function setInvestorRegistry(address _newAddress) checkAddress(_newAddress) external onlyAllowedManager('set_investor_repo') {
     InvestorRegistry = _newAddress;
     InvestorRegistryChangedEvent(_newAddress);
   }
 
-  function setCryDRRegistry(address _newAddress) checkAddress(_newAddress) onlyAllowedManager('set_crydr_repo') {
+  function setCryDRRegistry(address _newAddress) checkAddress(_newAddress) external onlyAllowedManager('set_crydr_repo') {
     CryDRRegistry = _newAddress;
     CryDRRegistryChangedEvent(_newAddress);
   }
@@ -89,19 +89,19 @@ contract JibrelAPI is JibrelAPIInterface, Manageable {
 
   /* Getters */
 
-  function getBODC() constant returns (address) {
+  function getBODC() external constant returns (address) {
     return BODC;
   }
 
-  function getJibrelDAO() constant returns (address) {
+  function getJibrelDAO() external constant returns (address) {
     return JibrelDAO;
   }
 
-  function getInvestorRegistry() constant returns (address) {
+  function getInvestorRegistry() external constant returns (address) {
     return InvestorRegistry;
   }
 
-  function getCryDRRegistry() constant returns (address) {
+  function getCryDRRegistry() external constant returns (address) {
     return CryDRRegistry;
   }
 
