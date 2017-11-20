1785b61b2c10afa5546a6da9f26b24b9e30169cc
Victor Mezrin <victor@mezrin.com>
2017-11-20 04:12:29 +0000

    remove ability to destruct the crydr controllers

--- contracts/crydr/controller/CrydrControllerBase.sol
+++ contracts/crydr/controller/CrydrControllerBase.sol
@@ -37,19 +37,7 @@ contract CrydrControllerBase is CrydrControllerBaseInterface,
   function CrydrControllerBase(uint _uuid) UUID(_uuid) {}
 
 
-  /* Selfdestruct */
-
-  /*
-   * @dev Clears all the contract's data it frees up space on the blockchain.
-   *      The ethers send to manager after selfdestruct call are lost.
-   */
-  function close() onlyAllowedManager('close_crydr_controller') {
-    // todo test it
-    selfdestruct(owner);
-  }
-
-
-  /* CrydrControllerInterface */
+  /* CrydrControllerBaseInterface */
 
   /* Configuration */
 
