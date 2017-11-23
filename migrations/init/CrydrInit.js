a52586ced96b7087f65d8ddb759c6d3392dd14ff
Victor Mezrin <victor@mezrin.com>
2017-11-23 07:31:43 +0000

    refactoring of init script

--- migrations/init/CrydrInit.js
+++ migrations/init/CrydrInit.js
@@ -36,15 +36,11 @@ export const configureCrydrManagers = async (crydrStorageAddress, crydrControlle
   return null;
 };
 
-export const linkCrydrContracts = async (crydrStorageAddress,
-                                         crydrControllerAddress,
-                                         crydrViewAddress,
-                                         crydrViewApiStandardName) => {
-  global.console.log('\tLink crydr contracts...');
+export const linkCrydrStorage = async (crydrStorageAddress,
+                                       crydrControllerAddress) => {
+  global.console.log('\tLink crydr storage to controller...');
   global.console.log(`\t\tcrydrStorageAddress - ${crydrStorageAddress}`);
   global.console.log(`\t\tcrydrControllerAddress - ${crydrControllerAddress}`);
-  global.console.log(`\t\tcrydrViewAddress - ${crydrViewAddress}`);
-  global.console.log(`\t\tcrydrViewApiStandardName - ${crydrViewApiStandardName}`);
 
   const { managerGeneral } = GlobalConfig.getAccounts();
   global.console.log(`\t\towner - ${managerGeneral}`);
@@ -54,12 +50,45 @@ export const linkCrydrContracts = async (crydrStorageAddress,
   await CrydrControllerBaseInterfaceJSAPI
     .setCrydrStorage(crydrControllerAddress, managerGeneral, crydrStorageAddress);
 
+  global.console.log('\tCrydr storage successfully linked');
+  return null;
+};
+
+export const linkCrydrView = async (crydrControllerAddress,
+                                    crydrViewAddress,
+                                    crydrViewApiStandardName) => {
+  global.console.log('\tLink crydr view to controller...');
+  global.console.log(`\t\tcrydrControllerAddress - ${crydrControllerAddress}`);
+  global.console.log(`\t\tcrydrViewAddress - ${crydrViewAddress}`);
+  global.console.log(`\t\tcrydrViewApiStandardName - ${crydrViewApiStandardName}`);
+
+  const { managerGeneral } = GlobalConfig.getAccounts();
+  global.console.log(`\t\tmanagerGeneral - ${managerGeneral}`);
+
   await CrydrViewBaseInterfaceJSAPI
     .setCrydrController(crydrViewAddress, managerGeneral, crydrControllerAddress);
   await CrydrControllerBaseInterfaceJSAPI
     .setCrydrView(crydrControllerAddress, managerGeneral, crydrViewAddress, crydrViewApiStandardName);
 
-  global.console.log('\tCrydr contracts successfully linked');
+  global.console.log('\tCrydr view successfully linked');
+  return null;
+};
+
+export const configureCrydr = async (crydrStorageAddress,
+                                     crydrControllerAddress,
+                                     crydrViewAddress,
+                                     crydrViewApiStandardName) => {
+  global.console.log('\tConfigure JCash crydr...');
+  global.console.log(`\t\tcrydrStorageAddress - ${crydrStorageAddress}`);
+  global.console.log(`\t\tcrydrControllerAddress - ${crydrControllerAddress}`);
+  global.console.log(`\t\tcrydrViewAddress - ${crydrViewAddress}`);
+  global.console.log(`\t\tcrydrViewApiStandardName - ${crydrViewApiStandardName}`);
+
+  await configureCrydrManagers(crydrStorageAddress, crydrControllerAddress, crydrViewAddress);
+  await linkCrydrStorage(crydrStorageAddress, crydrControllerAddress);
+  await linkCrydrView(crydrControllerAddress, crydrViewAddress, crydrViewApiStandardName);
+
+  global.console.log('\tJCash crydr successfully configured');
   return null;
 };
 
@@ -82,8 +111,10 @@ export const initCrydr = async (crydrStorageContractArtifact,
   const crydrViewInstance = await crydrViewContractArtifact.deployed();
   const crydrViewAddress = crydrViewInstance.address;
 
-  await configureCrydrManagers(crydrStorageAddress, crydrControllerAddress, crydrViewAddress);
-  await linkCrydrContracts(crydrStorageAddress, crydrControllerAddress, crydrViewAddress, crydrViewApiStandardName);
+  await configureCrydr(crydrStorageAddress,
+                       crydrControllerAddress,
+                       crydrViewAddress,
+                       crydrViewApiStandardName);
 
   global.console.log('\tJCash crydr successfully initialized');
   return null;
