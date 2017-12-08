f273c84299aac8aae865a10d322e11ac412f7904
Victor Mezrin <victor@mezrin.com>
2017-12-08 02:13:07 +0000

    set SafeMath funcs to pure

--- contracts/third-party/zeppelin-solidity/SafeMath.sol
+++ contracts/third-party/zeppelin-solidity/SafeMath.sol
@@ -6,25 +6,25 @@ pragma solidity ^0.4.18;
  * @dev Math operations with safety checks that throw on error
  */
 contract SafeMath {
-  function safeMul(uint256 a, uint256 b) internal constant returns (uint256) {
+  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function safeDiv(uint256 a, uint256 b) internal constant returns (uint256) {
+  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function safeSub(uint256 a, uint256 b) internal constant returns (uint256) {
+  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function safeAdd(uint256 a, uint256 b) internal constant returns (uint256) {
+  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
