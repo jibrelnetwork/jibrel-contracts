338c1f765a084924bdfdc98d3748adda81897cda
Victor Mezrin <victor@mezrin.com>
2017-12-08 01:28:00 +0000

    update truffle config

--- truffle.js
+++ truffle.js
@@ -8,26 +8,34 @@ module.exports = {
       host:       'localhost',
       port:       8560,
       network_id: '*', // Match any network id
-      gas:        4500000,
+      gas:        6700000,
     },
-    ropsten:     {
+    ropsten: {
       host:       'localhost',
       port:       8550,
       network_id: 3, // official id of the ropsten network
-      gas:        4500000,
+      gas:        4700000,
     },
-    main:        {
+    main: {
       host:       'localhost',
       port:       8545,
       network_id: 3, // official id of the ropsten network
-      gas:        4500000,
+      gas:        6700000,
     },
     coverage: {
-      host:       "localhost",
-      network_id: "*",
+      host:       'localhost',
+      network_id: '*',
       port:       8570,
       gas:        0xfffffffffff,
       gasPrice:   0x01,
-    }
+    },
   },
+  // uncomment if you want to deploy contracts to the ropsten
+  //
+  // solc: {
+  //   optimizer: {
+  //     enabled: true,
+  //     runs: 200,
+  //   },
+  // },
 };
