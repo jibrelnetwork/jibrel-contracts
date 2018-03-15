517bda97856eef33dd8c7254e2deee1642d89ad6
Victor Mezrin <victor@mezrin.com>
2018-03-15 16:16:16 +0000

    add JCashLicenseRegistry - template for asset contracts

---
+++ contracts/crydr/asset/JCashLicenseRegistry.sol
@@ -0,0 +1,28 @@
+/* Author: Victor Mezrin  victor@mezrin.com */
+
+pragma solidity ^0.4.18;
+
+
+import '../../feature/assetid/AssetID.sol';
+import '../../lifecycle/Ownable.sol';
+import '../../lifecycle/Manageable.sol';
+import '../../lifecycle/Pausable.sol';
+import '../../feature/bytecode/BytecodeExecutor.sol';
+import '../license/CrydrLicenseRegistry.sol';
+
+
+/**
+ * @title JCashLicenseRegistry
+ * @dev Contract that stores licenses
+ */
+contract JCashLicenseRegistry is AssetID,
+                                 Ownable,
+                                 Manageable,
+                                 Pausable,
+                                 BytecodeExecutor,
+                                 CrydrLicenseRegistry {
+
+  /* Constructor */
+
+  function JCashLicenseRegistry(string _assetID) AssetID(_assetID) public { }
+}
