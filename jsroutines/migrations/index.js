c500711afd7304d2029e0dc20786d8f26fdba596
Victor Mezrin <victor@mezrin.com>
2018-08-02 22:06:33 +0000

    remove jDemo

--- jsroutines/migrations/index.js
+++ jsroutines/migrations/index.js
@@ -12,18 +12,10 @@ const jKRWStorage    = global.artifacts.require('jKRWStorage.sol');
 const jKRWController = global.artifacts.require('jKRWController.sol');
 const jKRWViewERC20  = global.artifacts.require('jKRWViewERC20.sol');
 
-const jDemoStorage         = global.artifacts.require('jDemoStorage.sol');
-const jDemoLicenseRegistry = global.artifacts.require('jDemoLicenseRegistry.sol');
-const jDemoController      = global.artifacts.require('jDemoController.sol');
-const jDemoViewERC20       = global.artifacts.require('jDemoViewERC20.sol');
-
 const DeployConfig = require('../jsconfig/DeployConfig');
 const CrydrInit = require('../jsinit/CrydrInit');
 const CrydrControllerInit = require('../jsinit/CrydrControllerInit');
 const CrydrControllerMintableInterfaceJSAPI = require('../jsapi/crydr/controller/CrydrControllerMintableInterface');
-const CrydrControllerLicensedERC20JSAPI = require('../jsapi/crydr/controller/CrydrControllerLicensedERC20');
-const CrydrViewERC20NamedInterfaceJSAPI = require('../jsapi/crydr/view/CrydrViewERC20NamedInterface');
-const CrydrViewMetadataInterfaceJSAPI = require('../jsapi/crydr/view/CrydrViewMetadataInterface');
 
 
 /* Migration #2 */
@@ -153,94 +145,6 @@ const verifyMigrationNumber6 = async () => {
 };
 
 
-/* Migration #7 */
-
-export const executeMigrationNumber7 = async () => {
-  await CrydrInit.initLicensedCrydr(jDemoStorage, jDemoLicenseRegistry, jDemoController, jDemoViewERC20, 'erc20');
-
-  const jDemoStorageInstance = await jDemoStorage.deployed();
-  const jDemoStorageAddress = jDemoStorageInstance.address;
-  const jDemoLicenseRegistryInstance = await jDemoLicenseRegistry.deployed();
-  const jDemoLicenseRegistryAddress = jDemoLicenseRegistryInstance.address;
-  const jDemoControllerInstance = await jDemoController.deployed();
-  const jDemoControllerAddress = jDemoControllerInstance.address;
-  const jDemoViewERC20Instance = await jDemoViewERC20.deployed();
-  const jDemoViewERC20Address = jDemoViewERC20Instance.address;
-
-  await CrydrInit.upauseCrydrContract(jDemoStorage, 'storage');
-  await CrydrInit.upauseCrydrContract(jDemoLicenseRegistry, 'license_registry');
-  await CrydrInit.upauseCrydrContract(jDemoController, 'controller');
-  await CrydrInit.upauseCrydrContract(jDemoViewERC20, 'view');
-
-  global.console.log('  jDemo deployed, configured and unpaused:');
-  global.console.log(`\tjDemoStorageAddress: ${jDemoStorageAddress}`);
-  global.console.log(`\tjDemoLicenseRegistryAddress: ${jDemoLicenseRegistryAddress}`);
-  global.console.log(`\tjDemoControllerAddress: ${jDemoControllerAddress}`);
-  global.console.log(`\tjDemoViewERC20Address: ${jDemoViewERC20Address}`);
-};
-
-export const verifyMigrationNumber7 = async () => {
-  // todo verify migration, make integration tests
-};
-
-
-/* Migration #8 */
-
-const executeMigrationNumber8 = async () => {
-  const { managerGeneral, managerMint, managerLicense,
-          testInvestor1, testInvestor2, testInvestor3 } = DeployConfig.getAccounts();
-
-
-  global.console.log('  Grant licenses to users');
-
-  const licenseRegistryInstance = await jDemoLicenseRegistry.deployed();
-  const licenseRegistryAddress = licenseRegistryInstance.address;
-
-  const expirationTimestamp = Math.floor(Date.now() / 1000) + (365 * 24 * 60 * 60);
-
-  await Promise.all([CrydrControllerLicensedERC20JSAPI.licenseUser(licenseRegistryAddress, managerLicense,
-                                                                   testInvestor1, expirationTimestamp),
-                     CrydrControllerLicensedERC20JSAPI.licenseUser(licenseRegistryAddress, managerLicense,
-                                                                   testInvestor2, expirationTimestamp),
-                     CrydrControllerLicensedERC20JSAPI.licenseUser(licenseRegistryAddress, managerLicense,
-                                                                   testInvestor3, expirationTimestamp)]);
-
-
-  global.console.log('  Mint tokens');
-
-  const jDemoControllerInstance = await jDemoController.deployed();
-  const jDemoControllerAddress = jDemoControllerInstance.address;
-  const jDemoViewERC20Instance = await jDemoViewERC20.deployed();
-  const jDemoViewERC20Address = jDemoViewERC20Instance.address;
-
-  await Promise.all([
-    CrydrControllerMintableInterfaceJSAPI.mint(jDemoControllerAddress,
-                                               managerMint,
-                                               testInvestor1,
-                                               10000 * (10 ** 18)),
-    CrydrControllerMintableInterfaceJSAPI.mint(jDemoControllerAddress,
-                                               managerMint,
-                                               testInvestor2,
-                                               10000 * (10 ** 18)),
-    CrydrControllerMintableInterfaceJSAPI.mint(jDemoControllerAddress,
-                                               managerMint,
-                                               testInvestor3,
-                                               10000 * (10 ** 18)),
-  ]);
-
-
-  global.console.log('  Change CryDR metadata');
-  await CrydrViewERC20NamedInterfaceJSAPI.setSymbol(jDemoViewERC20Address, managerGeneral, 'JGLD');
-  await CrydrViewERC20NamedInterfaceJSAPI.setName(jDemoViewERC20Address, managerGeneral, 'jGold asset');
-  await CrydrViewMetadataInterfaceJSAPI.setMetadata(jDemoViewERC20Address, managerGeneral,
-                                                    'asset_type', 'commodity');
-};
-
-const verifyMigrationNumber8 = async () => {
-  // todo verify migration, make integration tests
-};
-
-
 /* Migrations */
 
 export const executeMigration = async (migrationNumber) => {
@@ -254,10 +158,6 @@ export const executeMigration = async (migrationNumber) => {
     await executeMigrationNumber5();
   } else if (migrationNumber === 6) {
     await executeMigrationNumber6();
-  } else if (migrationNumber === 7) {
-    await executeMigrationNumber7();
-  } else if (migrationNumber === 8) {
-    await executeMigrationNumber8();
   } else {
     throw new Error(`Unknown migration to execute: ${migrationNumber}`);
   }
@@ -274,10 +174,6 @@ export const verifyMigration = async (migrationNumber) => {
     await verifyMigrationNumber5();
   } else if (migrationNumber === 6) {
     await verifyMigrationNumber6();
-  } else if (migrationNumber === 7) {
-    await verifyMigrationNumber7();
-  } else if (migrationNumber === 8) {
-    await verifyMigrationNumber8();
   } else {
     throw new Error(`Unknown migration to execute: ${migrationNumber}`);
   }
