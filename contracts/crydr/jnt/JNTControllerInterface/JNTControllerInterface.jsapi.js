305a4862b73a6debc4dd792c3f1f4339936ed3e2
Victor Mezrin <victor@mezrin.com>
2018-08-06 10:30:05 +0000

    move and fix jsapi JNTController

---
+++ contracts/crydr/jnt/JNTControllerInterface/JNTControllerInterface.jsapi.js
@@ -0,0 +1,41 @@
+import { submitTxAndWaitConfirmation } from '../../../../jsroutines/util/SubmitTx';
+
+const Promise = require('bluebird');
+
+const JNTControllerInterfaceArtifact = global.artifacts.require('JNTControllerInterface.sol');
+
+
+/**
+ * Setters
+ */
+
+export const chargeJNT = async (crydrControllerAddress, managerAddress,
+                                fromAccount, toAccount, valueToCharge) => {
+  global.console.log('\tCharge JNT:');
+  global.console.log(`\t\tcrydrControllerAddress - ${crydrControllerAddress}`);
+  global.console.log(`\t\tmanagerAddress - ${managerAddress}`);
+  global.console.log(`\t\tfromAccount - ${fromAccount}`);
+  global.console.log(`\t\ttoAccount - ${toAccount}`);
+  global.console.log(`\t\tvalueToCharge - ${valueToCharge}`);
+  await submitTxAndWaitConfirmation(
+    JNTControllerInterfaceArtifact
+      .at(crydrControllerAddress)
+      .chargeJNT
+      .sendTransaction,
+    [fromAccount, toAccount, valueToCharge, { from: managerAddress }]);
+  global.console.log('\tJNT successfully charged');
+  return null;
+};
+
+
+/**
+ * Events
+ */
+
+export const getJNTChargedEvents = (contractAddress, eventDataFilter = {}, commonFilter = {}) => {
+  const eventObj = JNTControllerInterfaceArtifact
+    .at(contractAddress)
+    .JNTChargedEvent(eventDataFilter, commonFilter);
+  const eventGet = Promise.promisify(eventObj.get).bind(eventObj);
+  return eventGet();
+};
