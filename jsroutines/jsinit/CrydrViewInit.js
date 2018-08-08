bb8e302e69c53c484b2f216a91ae94f686eb9ad7
Victor Mezrin <victor@mezrin.com>
2018-08-08 14:21:05 +0000

    rework init scripts

--- jsroutines/jsinit/CrydrViewInit.js
+++ jsroutines/jsinit/CrydrViewInit.js
@@ -1,39 +1,35 @@
+// @flow
+
 import * as ManageableJSAPI from '../../contracts/lifecycle/Manageable/Manageable.jsapi';
 import * as PausableJSAPI from '../../contracts/lifecycle/Pausable/Pausable.jsapi';
 import * as CrydrViewBaseJSAPI from '../../contracts/crydr/view/CrydrViewBase/CrydrViewBase.jsapi';
 import * as CrydrViewERC20NamedJSAPI from '../../contracts/crydr/view/CrydrViewERC20Named/CrydrViewERC20Named.jsapi';
 
-import * as DeployConfig from '../jsconfig/DeployConfig';
-
+import { EthereumAccounts } from '../jsconfig/DeployConfig';
 import * as DeployUtils from '../util/DeployUtils';
 
 
-export const deployCrydrView = async (crydrViewContractArtifact, contractOwner) => {
+export const deployCrydrView = async (crydrViewContractArtifact, ethAccounts: EthereumAccounts) => {
   global.console.log('\tDeploying view of a crydr.');
 
-  const contractAddress = await DeployUtils.deployContractAndPersistArtifact(crydrViewContractArtifact, contractOwner);
+  const contractAddress = await DeployUtils.deployContractAndPersistArtifact(crydrViewContractArtifact, ethAccounts.owner);
 
   global.console.log(`\tView of a crydr successfully deployed: ${contractAddress}`);
   return null;
 };
 
-export const configureCrydrViewManagers = async (crydrViewAddress) => {
+export const configureCrydrViewManagers = async (crydrViewAddress, ethAccounts: EthereumAccounts) => {
   global.console.log('\tConfiguring managers of crydr view...');
   global.console.log(`\t\tcrydrViewAddress - ${crydrViewAddress}`);
 
-  const { owner, managerPause, managerGeneral } = DeployConfig.getAccounts();
-  global.console.log(`\t\towner - ${owner}`);
-  global.console.log(`\t\tmanagerPause - ${managerPause}`);
-  global.console.log(`\t\tmanagerGeneral - ${managerGeneral}`);
-
   await Promise.all(
     [
-      await PausableJSAPI.grantManagerPermissions(crydrViewAddress, owner, managerPause),
-      await ManageableJSAPI.enableManager(crydrViewAddress, owner, managerPause),
+      await PausableJSAPI.grantManagerPermissions(crydrViewAddress, ethAccounts.owner, ethAccounts.managerPause),
+      await ManageableJSAPI.enableManager(crydrViewAddress, ethAccounts.owner, ethAccounts.managerPause),
 
-      await CrydrViewBaseJSAPI.grantManagerPermissions(crydrViewAddress, owner, managerGeneral),
-      await CrydrViewERC20NamedJSAPI.grantManagerPermissions(crydrViewAddress, owner, managerGeneral),
-      await ManageableJSAPI.enableManager(crydrViewAddress, owner, managerGeneral),
+      await CrydrViewBaseJSAPI.grantManagerPermissions(crydrViewAddress, ethAccounts.owner, ethAccounts.managerGeneral),
+      await CrydrViewERC20NamedJSAPI.grantManagerPermissions(crydrViewAddress, ethAccounts.owner, ethAccounts.managerGeneral),
+      await ManageableJSAPI.enableManager(crydrViewAddress, ethAccounts.owner, ethAccounts.managerGeneral),
     ]
   );
 
