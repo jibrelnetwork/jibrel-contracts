99df9d947a01e3b55fede18895eddeaf17007545
Victor Mezrin <victor@mezrin.com>
2018-08-11 00:04:49 +0000

    uncomment tests

--- test/crydr/controller/CrydrControllerForcedTransfer.test.js
+++ test/crydr/controller/CrydrControllerForcedTransfer.test.js
@@ -4,6 +4,7 @@ import * as CrydrControllerBaseInterfaceJSAPI from '../../../contracts/crydr/con
 import * as CrydrControllerMintableInterfaceJSAPI from '../../../contracts/crydr/controller/CrydrControllerMintable/CrydrControllerMintableInterface.jsapi';
 import * as CrydrControllerForcedTransferInterfaceJSAPI from '../../../contracts/crydr/controller/CrydrControllerForcedTransfer/CrydrControllerForcedTransferInterface.jsapi';
 
+import * as CheckExceptions from '../../../jsroutines/util/CheckExceptions';
 import * as DeployConfig from '../../../jsroutines/jsconfig/DeployConfig';
 import * as CrydrStorageInit from '../../../jsroutines/jsinit/CrydrStorageInit';
 import * as CrydrControllerInit from '../../../jsroutines/jsinit/CrydrControllerInit';
@@ -111,47 +112,47 @@ global.contract('CrydrControllerForcedTransfer', (accounts) => {
     global.assert.strictEqual(balance.toNumber(), 1 * (10 ** 18),
                               'Expected that balance has increased due to forced transfer');
   });
-  //
-  // global.it('should test that functions throw if general conditions are not met', async () => {
-  //   global.console.log(`\tcrydrControllerInstance: ${crydrControllerInstance.address}`);
-  //   global.assert.notStrictEqual(crydrControllerInstance.address,
-  //                                '0x0000000000000000000000000000000000000000');
-  //
-  //   global.console.log(`\tcrydrStorageInstance: ${crydrStorageInstance.address}`);
-  //   global.assert.notStrictEqual(crydrStorageInstance.address,
-  //                                '0x0000000000000000000000000000000000000000');
-  //
-  //   global.console.log(`\tjcashCrydrViewERC20Instance: ${jcashCrydrViewERC20Instance.address}`);
-  //   global.assert.notStrictEqual(jcashCrydrViewERC20Instance.address,
-  //                                '0x0000000000000000000000000000000000000000');
-  //
-  //   const isPaused = await crydrControllerInstance.getPaused.call();
-  //   global.assert.strictEqual(isPaused, true,
-  //                             'Just configured crydrControllerBase contract must be paused');
-  //
-  //
-  //   let isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
-  //                                                         [0x0, 100 * (10 ** 18), { from: managerMint }]);
-  //   global.assert.strictEqual(isThrows, true, 'Should be a valid account address');
-  //
-  //   isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
-  //                                                     [ethAccounts.testInvestor1, 0, { from: managerMint }]);
-  //   global.assert.strictEqual(isThrows, true, 'Should be a positive value');
-  //
-  //   isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
-  //                                                     [ethAccounts.testInvestor1, 100 * (10 ** 18), { from: managerMint }]);
-  //   global.assert.strictEqual(isThrows, true, 'Only manager should be able to mint');
-  //
-  //   isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
-  //                                                     [0x0, 100 * (10 ** 18), { from: managerMint }]);
-  //   global.assert.strictEqual(isThrows, true, 'Should be a valid account address');
-  //
-  //   isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
-  //                                                     [ethAccounts.testInvestor1, 0, { from: managerMint }]);
-  //   global.assert.strictEqual(isThrows, true, 'Should be a positive value');
-  //
-  //   isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
-  //                                                     [ethAccounts.testInvestor1, 100 * (10 ** 18), { from: ethAccounts.testInvestor1 }]);
-  //   global.assert.strictEqual(isThrows, true, 'Only manager should be able to burn');
-  // });
+
+  global.it('should test that functions throw if general conditions are not met', async () => {
+    global.console.log(`\tcrydrControllerInstance: ${crydrControllerInstance.address}`);
+    global.assert.notStrictEqual(crydrControllerInstance.address,
+                                 '0x0000000000000000000000000000000000000000');
+
+    global.console.log(`\tcrydrStorageInstance: ${crydrStorageInstance.address}`);
+    global.assert.notStrictEqual(crydrStorageInstance.address,
+                                 '0x0000000000000000000000000000000000000000');
+
+    global.console.log(`\tjcashCrydrViewERC20Instance: ${jcashCrydrViewERC20Instance.address}`);
+    global.assert.notStrictEqual(jcashCrydrViewERC20Instance.address,
+                                 '0x0000000000000000000000000000000000000000');
+
+    const isPaused = await crydrControllerInstance.getPaused.call();
+    global.assert.strictEqual(isPaused, true,
+                              'Just configured crydrControllerBase contract must be paused');
+
+
+    let isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
+                                                          [0x0, 100 * (10 ** 18), { from: ethAccounts.managerMint }]);
+    global.assert.strictEqual(isThrows, true, 'Should be a valid account address');
+
+    isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
+                                                      [ethAccounts.testInvestor1, 0, { from: ethAccounts.managerMint }]);
+    global.assert.strictEqual(isThrows, true, 'Should be a positive value');
+
+    isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.mint.sendTransaction,
+                                                      [ethAccounts.testInvestor1, 100 * (10 ** 18), { from: ethAccounts.managerMint }]);
+    global.assert.strictEqual(isThrows, true, 'Only manager should be able to mint');
+
+    isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
+                                                      [0x0, 100 * (10 ** 18), { from: ethAccounts.managerMint }]);
+    global.assert.strictEqual(isThrows, true, 'Should be a valid account address');
+
+    isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
+                                                      [ethAccounts.testInvestor1, 0, { from: ethAccounts.managerMint }]);
+    global.assert.strictEqual(isThrows, true, 'Should be a positive value');
+
+    isThrows = await CheckExceptions.isContractThrows(crydrControllerInstance.burn.sendTransaction,
+                                                      [ethAccounts.testInvestor1, 100 * (10 ** 18), { from: ethAccounts.testInvestor1 }]);
+    global.assert.strictEqual(isThrows, true, 'Only manager should be able to burn');
+  });
 });
