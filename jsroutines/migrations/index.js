1b54b5f692c092c658720c1eeabce9e1c6f9f400
Victor Mezrin <victor@mezrin.com>
2018-08-11 07:50:19 +0000

    fix migration scripts

--- jsroutines/migrations/index.js
+++ jsroutines/migrations/index.js
@@ -39,13 +39,13 @@ const JcashRegistrarArtifact = global.artifacts.require('JcashRegistrar.sol');
 
 /* Migration #2 */
 
-export const executeMigrationNumber2 = async () => {
+const executeMigrationNumber2 = async () => {
   await CrydrInit.initCrydr(JNTStorageArtifact, JNTControllerArtifact, JNTViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
   await CrydrInit.upauseCrydrContract(JNTStorageArtifact, 'storage', DeployConfig.getEthAccounts());
   await CrydrInit.upauseCrydrContract(JNTControllerArtifact, 'controller', DeployConfig.getEthAccounts());
 };
 
-export const verifyMigrationNumber2 = async () => {
+const verifyMigrationNumber2 = async () => {
   // todo verify migration, make integration tests
 };
 
@@ -66,27 +66,23 @@ const verifyMigrationNumber3 = async () => {
 const executeMigrationNumber4 = async () => {
   /* JUSD */
 
-  await CrydrInit.initLicensedCrydr(JUSDStorageArtifact, JUSDLicenseRegistryArtifact, JUSDControllerArtifact, JUSDViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
+  const contractAddresses = await CrydrInit.initLicensedCrydr(JUSDStorageArtifact,
+                                                              JUSDLicenseRegistryArtifact,
+                                                              JUSDControllerArtifact,
+                                                              JUSDViewERC20Artifact,
+                                                              'erc20',
+                                                              DeployConfig.getEthAccounts());
 
-  await CrydrInit.upauseCrydrContract(JUSDStorageArtifact, 'storage', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JUSDLicenseRegistryArtifact, 'license_registry', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JUSDControllerArtifact, 'controller', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JUSDViewERC20Artifact, 'view', DeployConfig.getEthAccounts());
-
-  const JUSDStorageInstance = await JUSDStorageArtifact.deployed();
-  const JUSDStorageAddress = JUSDStorageInstance.address;
-  const JUSDLicenseRegistryInstance = await JUSDLicenseRegistryArtifact.deployed();
-  const JUSDLicenseRegistryAddress = JUSDLicenseRegistryInstance.address;
-  const JUSDControllerInstance = await JUSDControllerArtifact.deployed();
-  const JUSDControllerAddress = JUSDControllerInstance.address;
-  const JUSDViewERC20Instance = await JUSDViewERC20Artifact.deployed();
-  const JUSDViewERC20Address = JUSDViewERC20Instance.address;
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[0], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[1], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[2], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[3], DeployConfig.getEthAccounts().managerPause);
 
   global.console.log('  JUSD deployed, configured and unpaused:');
-  global.console.log(`\tJUSDStorageAddress: ${JUSDStorageAddress}`);
-  global.console.log(`\tJUSDLicenseRegistryAddress: ${JUSDLicenseRegistryAddress}`);
-  global.console.log(`\tJUSDControllerAddress: ${JUSDControllerAddress}`);
-  global.console.log(`\tJUSDViewERC20Address: ${JUSDViewERC20Address}`);
+  global.console.log(`\tJUSDStorageAddress: ${contractAddresses[0]}`);
+  global.console.log(`\tJUSDLicenseRegistryAddress: ${contractAddresses[3]}`);
+  global.console.log(`\tJUSDControllerAddress: ${contractAddresses[1]}`);
+  global.console.log(`\tJUSDViewERC20Address: ${contractAddresses[2]}`);
 };
 
 const verifyMigrationNumber4 = async () => {
@@ -99,27 +95,23 @@ const verifyMigrationNumber4 = async () => {
 const executeMigrationNumber5 = async () => {
   /* JEUR */
 
-  await CrydrInit.initLicensedCrydr(JEURStorageArtifact, JEURLicenseRegistryArtifact, JEURControllerArtifact, JEURViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
+  const contractAddresses = await CrydrInit.initLicensedCrydr(JEURStorageArtifact,
+                                                              JEURLicenseRegistryArtifact,
+                                                              JEURControllerArtifact,
+                                                              JEURViewERC20Artifact,
+                                                              'erc20',
+                                                              DeployConfig.getEthAccounts());
 
-  await CrydrInit.upauseCrydrContract(JEURStorageArtifact, 'storage', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JEURLicenseRegistryArtifact, 'license_registry', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JEURControllerArtifact, 'controller', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JEURViewERC20Artifact, 'view', DeployConfig.getEthAccounts());
-
-  const JEURStorageInstance = await JEURStorageArtifact.deployed();
-  const JEURStorageAddress = JEURStorageInstance.address;
-  const JEURLicenseRegistryInstance = await JEURLicenseRegistryArtifact.deployed();
-  const JEURLicenseRegistryAddress = JEURLicenseRegistryInstance.address;
-  const JEURControllerInstance = await JEURControllerArtifact.deployed();
-  const JEURControllerAddress = JEURControllerInstance.address;
-  const JEURViewERC20Instance = await JEURViewERC20Artifact.deployed();
-  const JEURViewERC20Address = JEURViewERC20Instance.address;
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[0], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[1], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[2], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[3], DeployConfig.getEthAccounts().managerPause);
 
   global.console.log('  JEUR deployed, configured and unpaused:');
-  global.console.log(`\tJEURStorageAddress: ${JEURStorageAddress}`);
-  global.console.log(`\tJEURLicenseRegistryAddress: ${JEURLicenseRegistryAddress}`);
-  global.console.log(`\tJEURControllerAddress: ${JEURControllerAddress}`);
-  global.console.log(`\tJEURViewERC20Address: ${JEURViewERC20Address}`);
+  global.console.log(`\tJEURStorageAddress: ${contractAddresses[0]}`);
+  global.console.log(`\tJEURLicenseRegistryAddress: ${contractAddresses[3]}`);
+  global.console.log(`\tJEURControllerAddress: ${contractAddresses[1]}`);
+  global.console.log(`\tJEURViewERC20Address: ${contractAddresses[2]}`);
 };
 
 const verifyMigrationNumber5 = async () => {
@@ -132,27 +124,23 @@ const verifyMigrationNumber5 = async () => {
 const executeMigrationNumber6 = async () => {
   /* JGBP */
 
-  await CrydrInit.initLicensedCrydr(JGBPStorageArtifact, JGBPLicenseRegistryArtifact, JGBPControllerArtifact, JGBPViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
+  const contractAddresses = await CrydrInit.initLicensedCrydr(JGBPStorageArtifact,
+                                                              JGBPLicenseRegistryArtifact,
+                                                              JGBPControllerArtifact,
+                                                              JGBPViewERC20Artifact,
+                                                              'erc20',
+                                                              DeployConfig.getEthAccounts());
 
-  await CrydrInit.upauseCrydrContract(JGBPStorageArtifact, 'storage', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JGBPLicenseRegistryArtifact, 'license_registry', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JGBPControllerArtifact, 'controller', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JGBPViewERC20Artifact, 'view', DeployConfig.getEthAccounts());
-
-  const JGBPStorageInstance = await JGBPStorageArtifact.deployed();
-  const JGBPStorageAddress = JGBPStorageInstance.address;
-  const JGBPLicenseRegistryInstance = await JGBPLicenseRegistryArtifact.deployed();
-  const JGBPLicenseRegistryAddress = JGBPLicenseRegistryInstance.address;
-  const JGBPControllerInstance = await JGBPControllerArtifact.deployed();
-  const JGBPControllerAddress = JGBPControllerInstance.address;
-  const JGBPViewERC20Instance = await JGBPViewERC20Artifact.deployed();
-  const JGBPViewERC20Address = JGBPViewERC20Instance.address;
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[0], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[1], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[2], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[3], DeployConfig.getEthAccounts().managerPause);
 
   global.console.log('  JGBP deployed, configured and unpaused:');
-  global.console.log(`\tJGBPStorageAddress: ${JGBPStorageAddress}`);
-  global.console.log(`\tJGBPLicenseRegistryAddress: ${JGBPLicenseRegistryAddress}`);
-  global.console.log(`\tJGBPControllerAddress: ${JGBPControllerAddress}`);
-  global.console.log(`\tJGBPViewERC20Address: ${JGBPViewERC20Address}`);
+  global.console.log(`\tJGBPStorageAddress: ${contractAddresses[0]}`);
+  global.console.log(`\tJGBPLicenseRegistryAddress: ${contractAddresses[3]}`);
+  global.console.log(`\tJGBPControllerAddress: ${contractAddresses[1]}`);
+  global.console.log(`\tJGBPViewERC20Address: ${contractAddresses[2]}`);
 };
 
 const verifyMigrationNumber6 = async () => {
@@ -165,27 +153,23 @@ const verifyMigrationNumber6 = async () => {
 const executeMigrationNumber7 = async () => {
   /* JKRW */
 
-  await CrydrInit.initLicensedCrydr(JKRWStorageArtifact, JKRWLicenseRegistryArtifact, JKRWControllerArtifact, JKRWViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
+  const contractAddresses = await CrydrInit.initLicensedCrydr(JKRWStorageArtifact,
+                                                              JKRWLicenseRegistryArtifact,
+                                                              JKRWControllerArtifact,
+                                                              JKRWViewERC20Artifact,
+                                                              'erc20',
+                                                              DeployConfig.getEthAccounts());
 
-  await CrydrInit.upauseCrydrContract(JKRWStorageArtifact, 'storage', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JKRWLicenseRegistryArtifact, 'license_registry', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JKRWControllerArtifact, 'controller', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JKRWViewERC20Artifact, 'view', DeployConfig.getEthAccounts());
-
-  const JKRWStorageInstance = await JKRWStorageArtifact.deployed();
-  const JKRWStorageAddress = JKRWStorageInstance.address;
-  const JKRWLicenseRegistryInstance = await JKRWLicenseRegistryArtifact.deployed();
-  const JKRWLicenseRegistryAddress = JKRWLicenseRegistryInstance.address;
-  const JKRWControllerInstance = await JKRWControllerArtifact.deployed();
-  const JKRWControllerAddress = JKRWControllerInstance.address;
-  const JKRWViewERC20Instance = await JKRWViewERC20Artifact.deployed();
-  const JKRWViewERC20Address = JKRWViewERC20Instance.address;
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[0], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[1], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[2], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[3], DeployConfig.getEthAccounts().managerPause);
 
   global.console.log('  JKRW deployed, configured and unpaused:');
-  global.console.log(`\tJKRWStorageAddress: ${JKRWStorageAddress}`);
-  global.console.log(`\tJKRWLicenseRegistryAddress: ${JKRWLicenseRegistryAddress}`);
-  global.console.log(`\tJKRWControllerAddress: ${JKRWControllerAddress}`);
-  global.console.log(`\tJKRWViewERC20Address: ${JKRWViewERC20Address}`);
+  global.console.log(`\tJKRWStorageAddress: ${contractAddresses[0]}`);
+  global.console.log(`\tJKRWLicenseRegistryAddress: ${contractAddresses[3]}`);
+  global.console.log(`\tJKRWControllerAddress: ${contractAddresses[1]}`);
+  global.console.log(`\tJKRWViewERC20Address: ${contractAddresses[2]}`);
 };
 
 const verifyMigrationNumber7 = async () => {
@@ -198,27 +182,23 @@ const verifyMigrationNumber7 = async () => {
 const executeMigrationNumber8 = async () => {
   /* JJOD */
 
-  await CrydrInit.initLicensedCrydr(JJODStorageArtifact, JJODLicenseRegistryArtifact, JJODControllerArtifact, JJODViewERC20Artifact, 'erc20', DeployConfig.getEthAccounts());
+  const contractAddresses = await CrydrInit.initLicensedCrydr(JJODStorageArtifact,
+                                                              JJODLicenseRegistryArtifact,
+                                                              JJODControllerArtifact,
+                                                              JJODViewERC20Artifact,
+                                                              'erc20',
+                                                              DeployConfig.getEthAccounts());
 
-  await CrydrInit.upauseCrydrContract(JJODStorageArtifact, 'storage', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JJODLicenseRegistryArtifact, 'license_registry', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JJODControllerArtifact, 'controller', DeployConfig.getEthAccounts());
-  await CrydrInit.upauseCrydrContract(JJODViewERC20Artifact, 'view', DeployConfig.getEthAccounts());
-
-  const JJODStorageInstance = await JJODStorageArtifact.deployed();
-  const JJODStorageAddress = JJODStorageInstance.address;
-  const JJODLicenseRegistryInstance = await JJODLicenseRegistryArtifact.deployed();
-  const JJODLicenseRegistryAddress = JJODLicenseRegistryInstance.address;
-  const JJODControllerInstance = await JJODControllerArtifact.deployed();
-  const JJODControllerAddress = JJODControllerInstance.address;
-  const JJODViewERC20Instance = await JJODViewERC20Artifact.deployed();
-  const JJODViewERC20Address = JJODViewERC20Instance.address;
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[0], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[1], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[2], DeployConfig.getEthAccounts().managerPause);
+  await PausableInterfaceJSAPI.unpauseContract(contractAddresses[3], DeployConfig.getEthAccounts().managerPause);
 
   global.console.log('  JJOD deployed, configured and unpaused:');
-  global.console.log(`\tJJODStorageAddress: ${JJODStorageAddress}`);
-  global.console.log(`\tJJODLicenseRegistryAddress: ${JJODLicenseRegistryAddress}`);
-  global.console.log(`\tJJODControllerAddress: ${JJODControllerAddress}`);
-  global.console.log(`\tJJODViewERC20Address: ${JJODViewERC20Address}`);
+  global.console.log(`\tJJODStorageAddress: ${contractAddresses[0]}`);
+  global.console.log(`\tJJODLicenseRegistryAddress: ${contractAddresses[3]}`);
+  global.console.log(`\tJJODControllerAddress: ${contractAddresses[1]}`);
+  global.console.log(`\tJJODViewERC20Address: ${contractAddresses[2]}`);
 };
 
 const verifyMigrationNumber8 = async () => {
