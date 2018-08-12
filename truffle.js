db412a5685db69942951c1c246f1993e52c42aba
Victor Mezrin <victor@mezrin.com>
2018-08-12 21:03:41 +0000

    adjust truffle gas limit

--- truffle.js
+++ truffle.js
@@ -10,37 +10,37 @@ module.exports = {
       host:       'localhost',
       port:       8560,
       network_id: '*', // Match any network id
-      gas:        8000000,
+      gas:        5600000,
     },
     development_hd: {
       provider:   new HDWalletProvider(Settings.mnemonic, 'http://localhost:8560', 0, 50),
       network_id: '*', // Match any network id
-      gas:        8000000,
+      gas:        5600000,
     },
     ropsten: {
       host:       'localhost',
       port:       8550,
       network_id: 3, // official id of the ropsten network
-      gas:        5500000,
+      gas:        5600000,
       gasPrice:   100000000000, // 100 Gwei
     },
     ropsten_hd: {
       provider:   new HDWalletProvider(Settings.mnemonic, 'http://localhost:8550', 0, 50),
       network_id: 3, // official id of the ropsten network
-      gas:        5500000,
+      gas:        5600000,
       gasPrice:   100000000000, // 100 Gwei
     },
     main: {
       host:       'localhost',
       port:       8545,
       network_id: 1, // official id of the main network
-      gas:        7000000,
+      gas:        5600000,
       gasPrice:   100000000000, // 100 Gwei
     },
     main_hd: {
       provider:   new HDWalletProvider(Settings.mnemonic, 'http://localhost:8545', 0, 50),
       network_id: 1, // official id of the main network
-      gas:        7000000,
+      gas:        5600000,
       gasPrice:   100000000000, // 100 Gwei
     },
     coverage: {
